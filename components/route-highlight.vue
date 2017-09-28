0a80f599dc5a59996a9be292ed916b3bc7c0ba57
Benjamin Canac <canacb1@gmail.com>
2017-09-28 20:47:47 +0200

    Fix curl hightlights with authorizations

--- components/route-highlight.vue
+++ components/route-highlight.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <pre v-highlightjs v-show="$store.state.language === 'shell'">
-      <code class="shell">curl -X{{route.method}} "{{url}}{{route.path}}" \<br><span v-if="route.session">-H "Authorization: ..."</span></code>
+      <code class="shell">curl -X{{route.method}} "{{url}}{{route.path}}"<span v-if="route.session"> \<br>-H "Authorization: ..."</span></code>
     </pre>
     <pre v-highlightjs v-show="$store.state.language === 'javascript'">
       <code class="javascript">const axios = require('axios')<br><br>await axios.{{route.method.toLowerCase()}}('{{route.path}}')</code>
