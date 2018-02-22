543f47c1c737ab6c79cf0563aa30e7ecb24a9137
Benjamin Canac <canacb1@gmail.com>
2018-02-22 16:42:20 +0100

    eslint: Udpate conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   env: {
     browser: true,
     node: true
   },
-  extends: 'standard',
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'vue'
   ],
   // add your custom rules here
   rules: {
@@ -18,5 +24,4 @@ module.exports = {
       asyncArrow: 'always'
     }],
   },
-  globals: {}
 }
